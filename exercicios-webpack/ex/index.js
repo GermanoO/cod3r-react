f55988c274a90fad87a7a170a4494a29ddc9eb69
germanoo <bgermano89@gmail.com>
2019-02-22 13:43:19 -0300

    first commit

---
+++ exercicios-webpack/ex/index.js
@@ -0,0 +1,29 @@
+import Pessoa from './pessoa'
+import './estilo.css'
+import 'react'
+
+export default props => (
+    <h1>Olá</h1>
+)
+
+console.log('Funcionou')
+
+
+// const pessoa = new Pessoa('Germano')
+// console.log(pessoa.toString())
+
+// const produto = {
+//     nome: 'Caneta Bic Preta',
+//     preco: 1.90,
+//     desconto: 0.05
+// }
+
+// function clone(objeto) {
+//     return { ...objeto}
+// }
+
+// const novoProduto = clone(produto)
+// novoProduto.nome = 'Caneta Bic Azul'
+
+// console.log(produto, novoProduto)
+
